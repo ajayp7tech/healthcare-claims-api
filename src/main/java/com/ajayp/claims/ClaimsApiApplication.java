22067398549f70b021c902b3eff8649ed49e2ba7
Ajay Pingali <ajayp.tech.777@gmail.com>
2026-05-23 11:23:12 -0500

    Create ClaimsApiApplication class for Spring Boot app

---
+++ src/main/java/com/ajayp/claims/ClaimsApiApplication.java
@@ -0,0 +1,11 @@
+package com.ajayp.claims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimsApiApplication.class, args);
+    }
+}
