622065d51d56cb20582105a70a65030f856ea82a
Ajay Pingali <ajayp.tech.777@gmail.com>
2026-05-23 11:34:01 -0500

    Create ClaimsApiApplicationTests.java

---
+++ src/test/java/com/ajayp/claims/ClaimsApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ajayp.claims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ClaimsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
