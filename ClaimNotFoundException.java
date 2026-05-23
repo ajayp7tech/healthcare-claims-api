ca72c12eeaf9d2b7bdfcd61da094d4867596ceb9
Ajay Pingali <ajayp.tech.777@gmail.com>
2026-05-23 11:03:38 -0500

    Delete ClaimNotFoundException.java

--- ClaimNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ajayp.claims.exception;
-
-public class ClaimNotFoundException extends RuntimeException {
-    public ClaimNotFoundException(String id) {
-        super("Claim not found: " + id);
-    }
-}
